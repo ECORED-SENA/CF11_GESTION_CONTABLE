7b5ee3e27f35bb8f06bfbe63d40bae4a433febc3
Daniel Mutis <64042203+DanielMutis85@users.noreply.github.com>
2021-07-13 09:43:48 -0500

    Video

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -619,16 +619,8 @@
     h3.mb-5.titulo-tercero Indemnizaciones      
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/Vo2C_w6H1pE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
       figcaption 
-
-
-
-
-
-
-
-
 
 
   
